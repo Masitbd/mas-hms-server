93ef7a5a9a9e8b8d8b6f6fecc5d83045f8972e90
maruf9270 <maruf9270@gmail.com>
2024-02-14 02:33:22 +0600

    Fixed all the ordering of the function on the controller and service model

--- src/app/modules/condition/condition.service.ts
+++ src/app/modules/condition/condition.service.ts
@@ -10,19 +10,6 @@ const postCondition = async (
   return result;
 };
 
-const getCondition = async () => {
-  const result = await Condition.find();
-  return result;
-};
-
-const getSingleCondition = async (id: string) => {
-  const result = await Condition.findOne({ _id: id });
-  if (!result) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Condition not Found');
-  }
-  return result;
-};
-
 const patchCondition = async (payload: Partial<ICondition>, id: string) => {
   const result = await Condition.findOneAndUpdate({ _id: id }, payload, {
     new: true,
@@ -34,10 +21,23 @@ const deleteCondition = async (id: string) => {
   const result = await Condition.findOneAndDelete({ _id: id });
   return result;
 };
+const fetchSingleCondition = async (id: string) => {
+  const result = await Condition.findOne({ _id: id });
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Condition not Found');
+  }
+  return result;
+};
+
+const fetchAllCondition = async () => {
+  const result = await Condition.find();
+  return result;
+};
+
 export const ConditionService = {
   postCondition,
-  getCondition,
-  getSingleCondition,
+  fetchAllCondition,
+  fetchSingleCondition,
   patchCondition,
   deleteCondition,
 };
