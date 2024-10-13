8471cfc9e2fc3e046be2edbc7229b65982bfde9e
git <maruf9270@gmail.com>
2024-10-13 22:27:45 +0600

    Modified patient fields

--- src/app/modules/patient/patient.model.ts
+++ src/app/modules/patient/patient.model.ts
@@ -1,4 +1,4 @@
-import { Schema, Types, model } from 'mongoose';
+import { Schema, model } from 'mongoose';
 import { patientIdGenerator } from '../../../utils/PatientIdGenerator';
 import { IPatient } from './patient.interface';
 
@@ -7,28 +7,25 @@ const PatientSchema = new Schema<IPatient>(
     name: { type: String, required: true },
     fatherName: { type: String, required: true },
     motherName: { type: String },
-    admissionDate: { type: String },
     district: { type: String },
     bloodGroup: { type: String },
-    courseDuration: { type: String },
+
     dateOfBirth: { type: String },
     maritalStatus: { type: String },
-    nationalID: { type: String },
-    passportNo: { type: String },
+
     nationality: { type: String },
     religion: { type: String },
-    totalAmount: { type: String },
-    typeOfDisease: { type: String },
+
     age: { type: String, required: true },
     gender: { type: String, required: true },
     presentAddress: { type: String, required: true },
     permanentAddress: { type: String },
     uuid: { type: String, unique: true },
-    ref_by: { type: Types.ObjectId, ref: 'doctor' },
-    consultant: { type: Types.ObjectId, ref: 'doctor' },
+
     phone: { type: String, required: true },
     email: { type: String },
     image: { type: String },
+    publicId: { type: String },
   },
   {
     timestamps: true,
--- src/app/modules/patient/patient.service.ts
+++ src/app/modules/patient/patient.service.ts
@@ -1,12 +1,32 @@
+import cloudinary from 'cloudinary';
+import httpStatus from 'http-status';
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
 import { IPatient } from './patient.interface';
 import { Patient } from './patient.model';
+
+cloudinary.v2.config({
+  cloud_name: config.claudinary_config.cloud_name,
+  api_key: config.claudinary_config.api_key,
+  api_secret: config.claudinary_config.api_secret,
+});
 
 const postPatient = async (params: IPatient) => {
   const result = await Patient.create(params);
   return result;
 };
 const patchPatient = async (params: IPatient) => {
+  const doesExists = await Patient.findOne({ uuid: params.uuid });
+  if (!doesExists) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Patient not found');
+  }
+
+  if (doesExists?.publicId && doesExists?.publicId !== params.publicId) {
+    await cloudinary.v2.uploader.destroy(doesExists.publicId);
+  }
+
   const result = await Patient.findOneAndUpdate({ uuid: params.uuid }, params);
+
   return result;
 };
 
